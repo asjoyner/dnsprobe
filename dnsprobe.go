efa24ee476af41c21d0da66087424e34ffa0d550
Aaron S. Joyner <aaron@joyner.ws>
2013-06-21 14:00:21 -0700

    Refactor data passing to a struct & method model
    Get rid of the globals
    Add some TODOs describing how we want to validate the slave responses
    Pave the way for also polling the master (next change)
    Query for TypeANY instead of TypeA, maybe we'll eventually grab the SOA?

--- dnsprobe.go
+++ dnsprobe.go
@@ -11,10 +11,17 @@ import (
   "github.com/tonnerre/godns"
 )
 
-// Define some globals that will get used over and over
-var dns_query *dns.Msg
+type DnsServer struct {
+  hostname, output_dir string
+  file *os.File
+  master bool
+  dns_client dns.Client
+  dns_query *dns.Msg
+  wait_group sync.WaitGroup
+}
 
-func query(dns_client *dns.Client, host string) []dns.RR {
+
+func query(dns_client dns.Client, host string, dns_query *dns.Msg) []dns.RR {
   resp, err := dns_client.Exchange(dns_query, host)
   if err != nil {
     fmt.Println(err)
@@ -25,46 +32,51 @@ func query(dns_client *dns.Client, host string) []dns.RR {
 
 
 // query once and write the output to the provided file handle
-func recordquery(dns_client *dns.Client, host string, f *os.File) {
+func (dns_server *DnsServer) recordquery() {
   t := time.Now()
-  // TODO: get the return value, sanity check it
-  query(dns_client, host)
+  // TODO: get the return value, compare with current value
+  // TODO: check_current_correct()
+  // TODO: if changed and configured: check_convergence()
+  query(dns_server.dns_client, dns_server.hostname, dns_server.dns_query)
   query_time := float64(time.Since(t) / time.Microsecond)
 
   text := fmt.Sprintf("%d %.3f\n", t.Unix(), query_time / 1000)
 
-  if _, err := f.WriteString(text); err != nil {
+  if _, err := dns_server.file.WriteString(text); err != nil {
     panic(err)
   }
 }
 
 
 // open the output file, loop forever polling the slave
-func pollslave(host string, output_dir string, wg sync.WaitGroup) {
-  filename := path.Join(output_dir, fmt.Sprintf("%v.data", host))
+func (dns_server *DnsServer) pollslave() {
+  filename := path.Join(dns_server.output_dir,
+                        fmt.Sprintf("%v.data", dns_server.hostname))
   f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
   if err != nil {
     log.Fatal("Could not write to the output file:", err)
   }
   defer f.Close()
+  dns_server.file = f
 
   dns_client := &dns.Client{}
   dns_client.ReadTimeout = 3 * time.Second  // TODO: 30 seconds
+  dns_server.dns_client = *dns_client
   for {
     // Schedule the wakeup before sending the query, so RTT doesn't cause skew
     sleepy_channel := time.After(5 * time.Second) // TODO: 300 seconds
-    recordquery(dns_client, host, f)
+    dns_server.recordquery()
     <-sleepy_channel
   }
-  wg.Done()
+  dns_server.wait_group.Done()
 }
 
 
 func main() {
   // Allocate those globals
-  dns_query = &dns.Msg{}
-  dns_query.SetQuestion("www.joyner.ws.", dns.TypeA)
-  var wg sync.WaitGroup
+  dns_query := dns.Msg{}
+  dns_query.SetQuestion("www.joyner.ws.", dns.TypeANY)
+  var wait_group sync.WaitGroup
 
   config_filehandle, err := os.Open("dnsprobe.cfg")
   if err != nil {
@@ -73,11 +85,16 @@ func main() {
 
   bufScanner := bufio.NewScanner(config_filehandle)
   for bufScanner.Scan() {
-    wg.Add(1)
-    go pollslave(bufScanner.Text(), "data", wg)
+    wait_group.Add(1)
+    dns_server := DnsServer{hostname: bufScanner.Text(),
+                            output_dir: "data",
+                            dns_query: &dns_query,
+                            wait_group: wait_group}
+
+    go dns_server.pollslave()
   }
 
-  wg.Wait()
+  wait_group.Wait()
   /*
   t := time.Now()
   resp1 := query("128.0.0.1:53")
