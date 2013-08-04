90fc369d901036094e1ac3f9c46101f7599d0dbc
Aaron S. Joyner <aaron@joyner.ws>
2013-08-04 09:38:22 -0700

    Add a script to setup a machine as a probe

---
+++ dnsprobe_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# copy the SSH keys into place
+
+
+# configure the git client:
+git config --global user.email probe1@gce
+git config --global user.name "GCE Probe1"
+
+# pull the latest copy of the dnsprobe binary?
+
+# sync down the dnsprobe-data project
+
