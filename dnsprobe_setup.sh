004e452a5259d7e3928e7dee3fc1395d3dec4446
Aaron S. Joyner <aaron@joyner.ws>
2013-08-07 14:27:30 -0400

    Some more meat to the setup script

--- dnsprobe_setup.sh
+++ dnsprobe_setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # copy the SSH keys into place
-
+cp id_rsa* ~/.ssh
 
 # configure the git client:
 git config --global user.email probe1@gce
 git config --global user.name "GCE Probe1"
 
-# pull the latest copy of the dnsprobe binary?
-
 # sync down the dnsprobe-data project
-
+git clone git@github.com:asjoyner/dnsprobe-data.git
